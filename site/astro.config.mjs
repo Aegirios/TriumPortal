db971e0c6622eadd436664d6fbbdca452da3c978
Aegirios <99557088+Aegirios@users.noreply.github.com>
2026-02-13 23:09:57 +0000

    correctif

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Aegirios.github.io/TriumPortal/',
+    base: '/TriumPortal/',
+});
